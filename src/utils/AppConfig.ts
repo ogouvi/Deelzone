204d1af640c62ddbf08a45c396f99433d2ad1725
Yaovi Ephraem Ogouvi <ogouviephraem@gmail.com>
2025-05-28 20:17:47 +0000

    new landing page

--- src/utils/AppConfig.ts
+++
@@ -1,6 +0,0 @@
-export const AppConfig = {
-  site_name: 'Deelz',
-  title: 'Deelzone, plateforme de revente',
-  description: 'Meilleur et la plugrande plateforme de revente au Togo ',
-  locale: 'fr',
-};
